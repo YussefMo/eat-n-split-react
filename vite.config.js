fa7c1f8145f8b390b4ff5410feeace7038c46b82
Youssef Mohammed <youssefapoulila@gmail.com>
2025-01-20 19:05:01 +0200

    update

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/[nameOfRepo]/",
+  base: "/eat-n-split-react/",
   build: {
     outDir: "dist",
   },
